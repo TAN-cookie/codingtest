f9241026c65d515eacbe6b549e1b236c70d262b6
TAN-cookie <mintrayvr@gmail.com>
2022-04-23 02:01:49 +0900

    create 내적
    내적 시작

---
+++ src/app/do_product/do_product.controller.js
@@ -0,0 +1,14 @@
+const router = require('express').Router()
+
+/**
+ * 내적
+ * @param {Array} a 숫자 배열
+ * @param {Array} b 숫자 배열
+ * @return {Number} 내적
+ */
+
+ function solution(a, b) {
+    var answer = 1234567890;
+    return answer;
+}
+module.exports = router;
